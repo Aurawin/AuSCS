1a995622db41b957a26a05ffe4cabf5c847f0ed7
Andrew <atbrunner@aurawin.com>
2015-09-28 15:39:48 -0500

    Imported breakout stored object from core

---
+++ src/main/java/com/aurawin/scs/stored/domain/network/Exposure.java
@@ -0,0 +1,7 @@
+package com.aurawin.scs.stored.domain.network;
+
+public class Exposure {
+    public static final byte None         = 0;
+    public static final byte Public       = 1;
+    public static final byte Private      = 2;
+}
